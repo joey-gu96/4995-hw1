7064c2dbde9015b3eeea0dfdeae4f8e6e788cb40
joey-gu96 <43620397+joey-gu96@users.noreply.github.com>
2019-02-05 23:54:58 -0500

    Update test_task13.py

--- task1/test_task13.py
+++ task1/test_task13.py
@@ -1,5 +1,5 @@
 import pandas as pd
 def test_task13():
-    data = pd.read_csv("input.txt", encoding = 'UTF-16',  index_col = 0, escapechar = '\\')
+    data = pd.read_csv("task1/input.txt", encoding = 'UTF-16',  index_col = 0, escapechar = '\\')
     assert data.shape[0] == 225
     assert data.shape[1] == 31
